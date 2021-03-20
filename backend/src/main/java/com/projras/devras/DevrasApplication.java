5d87583535eaf82937833b3634c6f19c477b0ece
Claudenir dos Santos <cstads2018@outlook.com>
2021-03-20 16:42:05 -0300

    Project created

---
+++ backend/src/main/java/com/projras/devras/DevrasApplication.java
@@ -0,0 +1,13 @@
+package com.projras.devras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevrasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevrasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/projras/devras/DevrasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projras.devras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevrasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
